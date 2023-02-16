9ee517713d28bbbab9174f5b104e8029dc9e4a44
lian <elisaxha@gmail.com>
2023-02-16 16:18:24 +0800

    完成管理員權限設置

--- index.php
+++ index.php
@@ -25,9 +25,26 @@
                                 <a href="?do=news">最新消息</a> |
                                 <a href="?do=look">購物流程</a> |
                                 <a href="?do=buycart">購物車</a> |
-                                <a href="?do=login">會員登入</a> |
-                                <a href="#" onclick="location.href='./api/logout.php?table=mem'">登出</a> |
-                                <a href="?do=admin">管理登入</a>
+                                <?php
+                                if(!isset($_SESSION['mem'])){
+                                ?>
+                                        <a href="?do=login">會員登入</a> |
+                                <?php
+                                }else{
+                                ?>
+                                        <a href="#" onclick="location.href='./api/logout.php?table=mem'">登出</a> |
+                                <?php
+                                }
+                                if(!isset($_SESSION['admin'])){
+                                ?>
+                                        <a href="?do=admin">管理登入</a>
+                                <?php
+                                }else{
+                                ?>
+                                        <a href="back.php?do=admin">返回管理</a>
+                                <?php
+                                }
+                                ?>
                         </div>
                         <marquee>情人節特惠活動 &nbsp;&nbsp;&nbsp;&nbsp; 為了慶祝七夕情人節，將舉辦情人兩人到現場有七七折之特惠活動~</marquee>
                 </div>
